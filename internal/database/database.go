e008a70853a3190993a36b39e16a5bc0a6540113
lhiradi <hiradjalilian@gmail.com>
2026-06-05 19:09:36 +0330

    Used SingleTone Design Pattern for DB Connection

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,23 +2,30 @@ package database
 
 import (
 	"proxmox-lxc-portal/internal/models"
+	"sync"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
+var (
+	DB   *gorm.DB
+	once sync.Once
+)
 
 func ConnectDB() {
-	db, err := gorm.Open(sqlite.Open("hosting.db"), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
+	once.Do(func() {
+		db, err := gorm.Open(sqlite.Open("hosting.db"), &gorm.Config{})
+		if err != nil {
+			panic("failed to connect database")
+		}
 
-	err = db.AutoMigrate(&models.User{}, &models.RefreshToken{})
-	if err != nil {
-		panic("failed to migrate database")
-	}
+		err = db.AutoMigrate(&models.User{}, &models.RefreshToken{})
+		if err != nil {
+			panic("failed to migrate database")
+		}
 
-	DB = db
+		DB = db
+	})
+
 }
